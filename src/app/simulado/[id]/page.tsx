263d5972923d44aed3725dcface671a42be6991b
Anderson Pianta <mrobotinfoms@gmail.com>
2025-11-25 08:04:50 -0400

    Delete src/app/simulado/[id] directory

--- src/app/simulado/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { PageProps } from '@/types';
-
-export default async function SimuladoPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  // resto do código...
-}
